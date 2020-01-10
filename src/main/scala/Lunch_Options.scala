b57d824c1855bee9169ce4fd98e8bfd1faa22a18
GerardGallagher <gerardgallagher@quantexa.com>
2020-01-10 11:31:44 +0000

    Drinks Addition

--- src/main/scala/Lunch_Options.scala
+++ src/main/scala/Lunch_Options.scala
@@ -3,4 +3,9 @@ object Lunch_Options {
   val option2 = "burger"
   val option3 = "pizza"
   val option4 = "hotdog"
+
+  val drink1 = "coke"
+  val drink2 = "sprite"
+  val drink3 = "fanta"
+  val drink4 = "drpepper"
 }
